9b1cd8c290f68ce0840e83d7887e85d052c2903a
trandaison <sontd.it@gmail.com>
2026-08-12 08:56:20 +0700

    chore(itfs): remove superseded @opencode-ai/itfs package and itfs-token-store

--- packages/itfs/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types.js"
-export * from "./session.js"
-export { ApiClient } from "./api-client.js"
-export * from "./tools.js"
-export * from "./interaction-log.js"
-export * from "./plugin.js"
